2ef02102c741b2c7926afd54cc73755f1f221a70
toto7545 <riviere.thomas@yahoo.fr>
2017-01-07 12:02:42 +0100

    pacman tunnel

--- src/Pacman.java
+++ src/Pacman.java
@@ -1,3 +1,6 @@
+/**
+ * Created by rivie_000 on 07/01/2017.
+ */
 public class Pacman {
 
     private static char dep;
@@ -8,48 +11,40 @@ public class Pacman {
     public static void setPos(int[] npos) {
         Pacman.npos = npos;
     }
-    public static int [] getPos(){
-        return npos;
-    }
 
     public static boolean Testdeplacement(boolean labybool[][]) {
         dep = Joueur.getDep();
         int x = npos[0];
         int y = npos[1];
-        res = true;
+        System.out.println(x + "=========" + y);
+        res = false;
         if (dep == 'z') {
             if (labybool[x - 1][y]) {
                 res = true;
-            }
-            if (!labybool[x - 1][y]) {
-                res = false;
             }
         }
         if (dep == 's') {
             if (labybool[x + 1][y]) {
                 res = true;
             }
-            if (!labybool[x + 1][y]) {
-                res = false;
-            }
         }
         if (dep == 'q') {
             if (labybool[x][y - 1]) {
                 res = true;
             }
-            if (!labybool[x][y - 1]) {
-                res = false;
+            if( x == 15 && y == 0){
+                res = true;
             }
         }
-
         if (dep == 'd') {
             if (labybool[x][y + 1]) {
                 res = true;
             }
-            if (!labybool[x][y + 1]) {
-                res = false;
+            if( x == 15 && y == 27){
+                res = true;
             }
         }
+        System.out.println(res);
         return res;
     }
     public static boolean getRes() {
@@ -75,7 +70,15 @@ public class Pacman {
         if (dep == 'd' && test == true) {
             npos[1] = npos[1] + 1;
         }
+
+        if(dep == 'q' && npos[0]==15 && npos[1] == 0 && test == true){
+            npos[1] = 27;
+        }
+        if(dep == 'd' && npos[0]==15 && npos[1] == 27 && test == true){
+            npos[1] = 0;
+        }
         erreur = false;
+
         if (test == false && ancienDep != dep) {
 
             if (ancienDep == 'z') {
@@ -85,10 +88,20 @@ public class Pacman {
                 npos[0] = npos[0] + 1;
             }
             if (ancienDep == 'q') {
-                npos[1] = npos[1] - 1;
+                if(npos[1]==0){
+                    npos[1] = 27;
+                }
+                else {
+                    npos[1] = npos[1] - 1;
+                }
             }
             if (ancienDep == 'd') {
-                npos[1] = npos[1] + 1;
+                if (npos[1] == 27){
+                    npos[1] = 0;
+                }
+                else {
+                    npos[1] = npos[1] + 1;
+                }
             }
             erreur = true;
         }
