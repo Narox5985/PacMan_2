bfabd031043c3ff1aad9438c807928928dc796c8
Narox5985 <ben5985.perrot@gmail.com>
2017-01-12 12:50:25 +0100

    modif

--- src/Scores.java
+++ src/Scores.java
@@ -46,7 +46,7 @@ private static int vic =0;
         int[][] caractF = Fantome.getCaractF();
 
         for (int k = 0; k < 4; k++) {
-            if ((caractF[2][k] == pos[1]) && (caractF[3][k] == pos[0])) {
+            if ((caractF[2][k] == pos[0]) && (caractF[3][k] == pos[1])) {
                 vies = vies - 1;
             }
         }
